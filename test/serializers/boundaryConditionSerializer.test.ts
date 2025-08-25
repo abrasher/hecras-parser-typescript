7b3de582065326431ed32bfc337583dff296a8e0
Amber <47009014+abrasher@users.noreply.github.com>
2025-08-25 10:59:19 -0400

    Fix boundary condition serializer test

--- test/serializers/boundaryConditionSerializer.test.ts
+++ test/serializers/boundaryConditionSerializer.test.ts
@@ -32,7 +32,7 @@ describe("BoundaryConditionSerializer", () => {
       expect(result[2]).toBe("BC Line Start Position= 485748.087453498 , 4751198.7455208 ")
       expect(result[3]).toBe("BC Line Middle Position= 485779.453690181 , 4751203.15089112 ")
       expect(result[4]).toBe("BC Line End Position= 485810.819926863 , 4751207.55626144 ")
-      expect(result[5]).toBe("BC Line Arc=2") // this might be actually "BC Line Arc= 2 ", check later
+      expect(result[5]).toBe("BC Line Arc= 2 ") // this might be actually "BC Line Arc= 2 ", check later
       expect(result[6]).toBe("485748.087453498 4751198.7455208485810.8199268634751207.55626144")
       expect(result[7]).toBe("BC Line Text Position= 1.79769313486232E+308 , 1.79769313486232E+308 ")
     })
