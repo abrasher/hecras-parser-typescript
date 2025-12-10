0c936c82d398a9c5884308f77bb90e7de74758e2
Amber <47009014+abrasher@users.noreply.github.com>
2025-12-10 17:48:55 -0500

    add padding

--- src/schemas/planSchema.ts
+++ src/schemas/planSchema.ts
@@ -32,26 +32,30 @@ export const planSchema = schema([
   stringField("flowRegime", "", { trim: true }), // "Subcritical Flow", "Supercritical Flow", or "Mixed Flow Regime"
 
   booleanField("kSumByGR", "K Sum by GR=", { pad: true, mode: "-1,0" }),
-  numberField("stdStepTol", "Std Step Tol="),
-  numberField("criticalTol", "Critical Tol="),
-  numberField("numOfStdStepTrials", "Num of Std Step Trials=", { integer: true }),
-  numberField("maxErrorTol", "Max Error Tol="),
-  numberField("flowTolRatio", "Flow Tol Ratio="),
-  numberField("splitFlowNTrial", "Split Flow NTrial=", { integer: true }),
-  numberField("splitFlowTol", "Split Flow Tol="),
-  numberField("splitFlowRatio", "Split Flow Ratio="),
-  numberField("logOutputLevel", "Log Output Level=", { integer: true }),
-  numberField("frictionSlopeMethod", "Friction Slope Method=", { integer: true }),
-  numberField("unsteadyFrictionSlopeMethod", "Unsteady Friction Slope Method=", { integer: true }),
+  numberField("stdStepTol", "Std Step Tol=", { pad: true }),
+  numberField("criticalTol", "Critical Tol=", { pad: true }),
+  numberField("numOfStdStepTrials", "Num of Std Step Trials=", { integer: true, pad: true }),
+  numberField("maxErrorTol", "Max Error Tol=", { pad: true }),
+  numberField("flowTolRatio", "Flow Tol Ratio=", { pad: true }),
+  numberField("splitFlowNTrial", "Split Flow NTrial=", { integer: true, pad: true }),
+  numberField("splitFlowTol", "Split Flow Tol=", { pad: true }),
+  numberField("splitFlowRatio", "Split Flow Ratio=", { pad: true }),
+  numberField("logOutputLevel", "Log Output Level=", { integer: true, pad: true }),
+  numberField("frictionSlopeMethod", "Friction Slope Method=", { integer: true, pad: true }),
+  numberField("unsteadyFrictionSlopeMethod", "Unsteady Friction Slope Method=", {
+    integer: true,
+    pad: true,
+  }),
   numberField("unsteadyBridgesFrictionSlopeMethod", "Unsteady Bridges Friction Slope Method=", {
     integer: true,
+    pad: true,
   }),
 
   stringField("parabolicCriticalDepth", "", { trim: true, optional: true }), // "Parabolic Critical Depth" if present
 
   // GlobalFlagsAndEncroachment section
   tupleField("globalVelDist", "Global Vel Dist=", [numberPart(), numberPart(), numberPart()]),
-  numberField("globalLogLevel", "Global Log Level=", { integer: true }),
+  numberField("globalLogLevel", "Global Log Level=", { integer: true, pad: true }),
   booleanField("checkData", "CheckData=", { mode: "trueFalse" }),
   tupleField("encroachParam", "Encroach Param=", [
     numberPart(),
