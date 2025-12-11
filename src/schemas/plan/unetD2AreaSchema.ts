e2d8b26d4cfd6aadae7db86a9255cf890ac039b6
Amber <47009014+abrasher@users.noreply.github.com>
2025-12-10 22:36:06 -0500

    further

--- src/schemas/plan/unetD2AreaSchema.ts
+++ src/schemas/plan/unetD2AreaSchema.ts
@@ -1,7 +1,7 @@
 import { numberField, schema, stringField } from "../../schema"
 
 export const unetD2AreaSchema = schema([
-  stringField("name", "UNET D2 Name=", { length: 16, trim: true }),
+  stringField("name", "UNET D2 Name=", { trim: false }),
   numberField("theta", "UNET D2 Theta=", { pad: true }),
   numberField("thetaWarmup", "UNET D2 Theta Warmup=", { pad: true }),
   numberField("zTol", "UNET D2 Z Tol=", { pad: true }),
@@ -16,7 +16,10 @@ export const unetD2AreaSchema = schema([
     nullOnBlank: true,
     optional: true,
   }),
-  numberField("smagorinskyMixing", "UNET D2 Smagorinsky Mixing=", { nullOnBlank: true, optional: true }),
+  numberField("smagorinskyMixing", "UNET D2 Smagorinsky Mixing=", {
+    nullOnBlank: true,
+    optional: true,
+  }),
   numberField("bcVolumeCheck", "UNET D2 BCVolumeCheck=", { integer: true }),
   numberField("latitude", "UNET D2 Latitude=", { nullOnBlank: true }),
   numberField("cores", "UNET D2 Cores=", { integer: true, optional: true }),
